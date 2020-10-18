cb6b69cb0669fe87268b8e4a1ffb7e8087bdbf96
justin <justinphil02@yahoo.com>
2020-10-18 16:01:39 -0700

    add Era, Budget, and Results components. Build out Container Component.

--- src/containers/Container.js
+++ src/containers/Container.js
@@ -3,6 +3,9 @@ import '../Test.css'
 import Welcome from '../components/Welcome'
 import RelationshipStatus from '../components/RelationshipStatus'
 import DateType from '../components/DateType'
+import Era from '../components/Era'
+import Budget from '../components/Budget'
+import Results from '../containers/Results'
 
 
 
@@ -47,7 +50,11 @@ class Container extends Component {
     } else if (this.state.stage === "dateType") {
         return ( <DateType handleClick={this.handleDateTypeSubmit} /> )
     } else if (this.state.stage === "era") {
-        return ( <h1>ERA type: {this.state.dateType}</h1> )
+        return ( <Era /> )
+    } else if (this.state.stage === "budget") {
+        return ( <Budget /> )
+    } else if (this.state.stage === "results") {
+        return ( <Results /> )
     }
     
   };
