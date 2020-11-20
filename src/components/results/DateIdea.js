dd8d268496553b87efb78b71fdf9fc4cd9235dee
Justin <justinphil02@yahoo.com>
2020-11-20 15:35:11 -0800

    add DateIdea and YelpResult to results folder. Remove YelpTest component from Results Container

---
+++ src/components/results/DateIdea.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const DateIdea = (props) => {return(<div className="card">{props.name}</div>)}
+
+export default DateIdea
--- src/containers/Results.js
+++ src/containers/Results.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import heartlogo from '../assets/heartlogo.png'
 //import {useBusinessSearch} from '../hooks/yelp-api/useBusinessSearch'
-import YelpTest from '../components/YelpTest'
 import {dateOptions} from '../components/results/DateOptions'
+import DateIdea from '../components/results/DateIdea'
+//import YelpResult from '../components/results/YelpResult'
 
 export default function Results(props) {
   const optionsArray = dateOptions(props)
+  const dateIdeaList = optionsArray.map((x) => <DateIdea name={x} />)
+  //const yelpResultList = <YelpResult />
   //const term = "Dinner"
   //const locationParam = "Oakland"
   //const [businesses, searchParams, setSearchParams] = useBusinessSearch(term, locationParam);  
+
   return(
       <div className="container">
         
           <img src={heartlogo} alt="heart logo" className="heartlogo" />
-          
-          <YelpTest businesses={optionsArray}/>
+          {dateIdeaList}
           
       </div>
   )
