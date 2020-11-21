c95b33481113c4e437855dc4ae094df91071558d
Justin <justinphil02@yahoo.com>
2020-11-21 10:28:31 -0800

    format results. display either date options or yelp results at one time

--- src/containers/Results.js
+++ src/containers/Results.js
@@ -6,32 +6,56 @@ import DateIdea from '../components/results/DateIdea'
 import YelpResult from '../components/results/YelpResult'
 
 export default function Results(props) {
-  const [dateSelected, setDateSelected] = useState("Theatre");
-  const select = () => setDateSelected("new value")
+  const [dateSelected, setDateSelected] = useState("initialize");
+  const select = () => setDateSelected("something")
+  const back = () => setDateSelected("initialize")
 
   const optionsArray = dateOptions(props)
-  const dateIdeaList = optionsArray.map((x) => <div><DateIdea key={x} name={x} select={select}/></div>)
+  const dateIdeaList = optionsArray.map((x) => <DateIdea key={x} name={x} select={select}/>)
   
-  const term = dateSelected
+  const term = "Theatre"
   const locationParam = "Oakland"
   const [businesses, searchParams, setSearchParams] = useBusinessSearch(term, locationParam);  
-  const yelpResultList = businesses.map((x) => <YelpResult key={Math.random() * 1000} business={x} />)
+  const yelpResultList = businesses.map((x) => <YelpResult key={x.id} business={x} />)
+  
+  /*
+  Goal:
+  On Results page Load, Display Date Options and 
+  Hide Yelp Results Until a Date Idea Selection is made 
 
+  When a selection is made, display yelp results
+  Add a back button to display add date ideas
+  
+  *** Need to Look up how to update use setSearchParams *** 
+  When a date selection is made, update Yelp Search 
+  */
+  if (dateSelected === "initialize"){
+    return (
+      <div className="container">
+        
+      <img src={heartlogo} alt="heart logo" className="heartlogo" />
+      <div className="date-idea-div">
+        <h1>Date Ideas</h1>        
+        {dateIdeaList}
+      </div>
+
+  </div>
+    )
+  } else {
   return(
       <div className="container">
         
           <img src={heartlogo} alt="heart logo" className="heartlogo" />
-          <div className="date-idea-div">
-            <h1>Date Ideas</h1>
-            {dateIdeaList}
-          </div>
+        
           <div className="date-idea-div">
             <h1>Results</h1>
+            <button  className="button" onClick={back}>Go Back</button><br />
+            <span>Search Params - {searchParams.term}, {searchParams.location}</span>
             {yelpResultList}
           </div>
 
           
       </div>
-  )
+  ) }
 }
 
