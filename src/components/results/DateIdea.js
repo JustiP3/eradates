167f792afac48364bb6131464a3954d0d0aeca5d
Justin <justinphil02@yahoo.com>
2020-11-21 08:49:55 -0800

    add useState hook to Results

--- src/components/results/DateIdea.js
+++ src/components/results/DateIdea.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const DateIdea = (props) => {return(<div className="card">{props.name}</div>)}
+const DateIdea = (props) => {return(<div onClick={props.select} className="date-options-card">{props.name}</div>)}
 
 export default DateIdea
--- src/containers/Results.js
+++ src/containers/Results.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import heartlogo from '../assets/heartlogo.png'
 import {useBusinessSearch} from '../hooks/yelp-api/useBusinessSearch'
 import {dateOptions} from '../components/results/DateOptions'
@@ -6,10 +6,13 @@ import DateIdea from '../components/results/DateIdea'
 import YelpResult from '../components/results/YelpResult'
 
 export default function Results(props) {
+  const [dateSelected, setDateSelected] = useState("Theatre");
+  const select = () => setDateSelected("new value")
+
   const optionsArray = dateOptions(props)
-  const dateIdeaList = optionsArray.map((x) => <DateIdea key={Math.random() * 1000} name={x} />)
+  const dateIdeaList = optionsArray.map((x) => <div><DateIdea key={x} name={x} select={select}/></div>)
   
-  const term = "Dinner"
+  const term = dateSelected
   const locationParam = "Oakland"
   const [businesses, searchParams, setSearchParams] = useBusinessSearch(term, locationParam);  
   const yelpResultList = businesses.map((x) => <YelpResult key={Math.random() * 1000} business={x} />)
@@ -20,6 +23,7 @@ export default function Results(props) {
           <img src={heartlogo} alt="heart logo" className="heartlogo" />
           {dateIdeaList}
           {yelpResultList}
+
           
       </div>
   )
