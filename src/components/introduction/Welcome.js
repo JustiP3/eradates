47cd28bdbda7fa8ddf997fcae0e36f87f5ec5352
justin <justinphil02@yahoo.com>
2021-01-27 11:29:59 -0800

    add admin menu button to home. only display testcomponent when admin menu is true

--- src/components/introduction/Welcome.js
+++ src/components/introduction/Welcome.js
@@ -1,20 +1,36 @@
 import Bounce from 'react-reveal/Bounce';
-import React from 'react'
+import React, { useState } from 'react'
 import heartlogo from '../../assets/heartlogo.png'
 import TestComponent from './TestComponent.js'
 
 const Welcome = (props) => {
+    const [adminMenu, setAdminMenu] = useState(false)
 
-    return(
-        <div className="welcome-container" >  
-            <img src={heartlogo} alt="heart logo" className="heartlogo" />
-            <Bounce top>
-                <h1 onClick={props.handleWelcomeClick}>Welcome</h1>
-            </Bounce> 
-            <TestComponent />
-        </div>
+    const handleAdminClick = () => setAdminMenu(true)
+    const handleBackClick = () => setAdminMenu(false)
 
-    )
+    if (adminMenu) {
+        return(
+            <div className="welcome-container" >  
+                <img src={heartlogo} alt="heart logo" className="heartlogo" />
+                <Bounce top>
+                    <TestComponent />
+                </Bounce> 
+                <button onClick={handleBackClick}>Back</button>
+            </div>    
+        )
+    } else {
+        return(
+            <div className="welcome-container" >  
+                <img src={heartlogo} alt="heart logo" className="heartlogo" />
+                <Bounce top>
+                    <h1 onClick={props.handleWelcomeClick}>Welcome</h1>
+                </Bounce> 
+                <button onClick={handleAdminClick}>Admin Menu</button>
+            </div>    
+        )
+    }
+    
 
 }
 
