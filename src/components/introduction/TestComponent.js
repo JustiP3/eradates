db05a059b7ffdfdb73b81049ec0abfbf502c29dd
justin <justinphil02@yahoo.com>
2021-01-27 11:22:33 -0800

    update TestComponent to display all dates in database.

--- src/components/introduction/TestComponent.js
+++ src/components/introduction/TestComponent.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 
-const TestComponent = (props) => {  
-    
-    const testRequest = () => {
+export default class TestComponent extends Component {
+    state = {
+        dateName: "",
+        safe: "false",
+        era: "",
+        viewAll: false,
+        allDates: []
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:9000/api/datetypes').then(
+            response => response.json()).then(
+                data => {
+                    if (data.message === 'success') {
+                        this.setState({...this.state, allDates: data.data})
+                    } else {
+                        console.log('error fetching datetypes')
+                    }
+                    
+                }
+            )
+        
+    }
+
+    handleShowAll = () => {
+        console.log(this.state.allDates)
+        this.setState({...this.state, viewAll: true}) 
+        
+    }
+
+    handleHide = () => {
+        this.setState({...this.state, viewAll: false})
+    }
+
+    testRequest = () => {
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -15,12 +47,20 @@ const TestComponent = (props) => {
         }
 
 
+        render() {
 
-    return(
-        <div>  
-            <button onClick={testRequest}>Click click</button>  
-        </div>
-    )
-}
+            if (this.state.viewAll === false) {
+                return(<div> <button onClick={this.handleShowAll}>Show All Dates</button></div>)
+            } else {
+                const allDatesList = this.state.allDates.map(x => <p>{x.id} - {x.name} - safe? {x.safe}</p>)
+                return(<div>${allDatesList}<button onClick={this.handleHide}>Hide Dates</button></div>)
+            }
+            
+        }
+} 
+    
+    
 
-export default TestComponent
+
+   
+
