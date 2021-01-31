f0b2a90e856083d4ac5c125082412a8dfd4bd582
justin <justinphil02@yahoo.com>
2021-01-31 15:01:22 -0800

    useeffect to fetch dates from server in the results container is not working

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 TODO LIST
 
-- 2 forms for add date - create date and assocaite date with era
-
-- need to update submit form function for both, need to complete assocate date form.
-
-- need to update results components with content from server
+- **_ need to update results components with content from server _**
 
 - In YelpResult, add option to view details about a business (location, etc.)
 
--- src/containers/Results.js
+++ src/containers/Results.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {useBusinessSearch} from '../hooks/yelp-api/useBusinessSearch'
 import {dateOptions} from '../components/results/DateOptions'
 import DateOptionsList from '../components/results/DateOptionsList'
@@ -6,6 +6,12 @@ import YelpResultsList from '../components/results/YelpResultsList'
 
 export default function Results(props) {
   const [dateSelected, setDateSelected] = useState("initialize");
+  const [allDates, setAllDates] = useState([]);
+  const [allEraDates, setAllEraDates] = useState([]);
+
+  useEffect(() => {
+    fetchDateTypes()
+  })
   const select = (value) => {
     setDateSelected(value)
     setSearchParams({term: value, location: {latitude: props.latitude, longitude: props.longitude}}) // *** need to add parameters term,location
@@ -16,8 +22,35 @@ export default function Results(props) {
   const [businesses, setSearchParams] = useBusinessSearch(dateSelected, {latitude: props.latitude, longitude: props.longitude}); 
   
 
+  const fetchDateTypes = () => {
+    return fetch('http://localhost:9000/api/datetypes').then(
+        response => response.json()).then(
+            data => {
+                if (data.message === 'success') {
+                    console.log(data)
+                    setAllDates(data.data)                    
+                } else {
+                    console.log('error fetching datetypes')                    
+                }                    
+            })
+            .then(fetchErasDates())
+}
+
+const fetchErasDates = () => {
+    return fetch('http://localhost:9000/api/eras').then(
+        response => response.json()).then(
+            data => {
+                if (data.message === 'success') {
+                    console.log(data)
+                    setAllEraDates(data.data)
+                } else {
+                    console.log('error fetching era date associations')
+                }                    
+            })
+}
+
   if (dateSelected === "initialize"){    
-    return (<DateOptionsList options={optionsArray} select={select} /> )
+    return (<DateOptionsList options={allDates} select={select} /> )
   } else {  //dateSelected == user Selection   
     return(<YelpResultsList businesses={businesses} dateSelected={dateSelected} back={back} />)
     }
