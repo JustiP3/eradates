31a708681f10671df0faf73532b79e880029ad98
justin <justinphil02@yahoo.com>
2021-01-31 12:15:46 -0800

    spit add date form and associate form into two components.

---
+++ src/components/admin/AssociateForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AssociateForm = (props) => {
+
+    return(
+    <div className='container'> 
+        Associate Form test content            
+    </div>
+    )
+}
+
+export default AssociateForm
--- src/containers/Admin.js
+++ src/containers/Admin.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import DateTypeList from '../components/admin/DateTypesList'
 import AddDateTypeForm from '../components/admin/AddDateTypeForm'
+import AssociateForm from '../components/admin/AssociateForm'
 
 
 export default class Admin extends Component {
@@ -65,68 +66,44 @@ export default class Admin extends Component {
     handleSubmit = (event) => {
         
         event.preventDefault();
-        console.log("submit clicked")
-        console.log(this.state.dateName)
-        console.log(this.state.era)
-        console.log(this.state.safe)
+        
         if (this.state.dateName !== "") {
             const datetypesRequestOptions = {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ dateName: this.state.dateName, safe: this.state.safe})
             };
-            const eradatesRequestOptions = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ type: this.state.dateName, era: this.state.era })
-            }
+            
             fetch('http://localhost:9000/api/datetypes', datetypesRequestOptions)
             .then(response => response.json())
             .then(data => {
-                console.log(data)                
-            }).then(
-                fetch(`/api/eras/${this.state.era}/${this.state.dateName}`, eradatesRequestOptions)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
+                console.log(data)   
                 if (data.status === 'success') {
                     this.fetchDateTypes();
-                }
-            }))
+                }             
+            })              
         }
     }
 
     handleAssociateSubmit = (event) => {
         
         event.preventDefault();
-        console.log("submit clicked")
-        console.log(this.state.dateName)
-        console.log(this.state.era)
-        console.log(this.state.safe)
+
         if (this.state.dateName !== "") {
-            const datetypesRequestOptions = {
+            const requestOptions = {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ dateName: this.state.dateName, safe: this.state.safe})
+                body: JSON.stringify({ type: this.state.dateName, era: this.state.era})
             };
-            const eradatesRequestOptions = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ type: this.state.dateName, era: this.state.era })
-            }
-            fetch('http://localhost:9000/api/datetypes', datetypesRequestOptions)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)                
-            }).then(
-                fetch(`/api/eras/${this.state.era}/${this.state.dateName}`, eradatesRequestOptions)
+           
+            fetch(`http://localhost:9000/api/eras/${this.state.era}/${this.state.dateName}`, requestOptions)
             .then(response => response.json())
             .then(data => {
                 console.log(data)
                 if (data.status === 'success') {
                     this.fetchDateTypes();
                 }
-            }))
+            })
         }
     }
 /*
@@ -146,12 +123,20 @@ export default class Admin extends Component {
             if (this.state.viewAll === false) {
                 return(
                     <div className='container'>
-                         <AddDateTypeForm 
-                            handleSubmit={this.handleSubmit} 
-                            handleInputChange={this.handleInputChange}
-                            handleShowAll={this.handleShowAll}
-                            exitAdminMenu={this.exitAdminMenu} 
-                            handleAssociateSubmit={this.handleAssociateSubmit}/> 
+                        <div className='card'>
+                            <AddDateTypeForm 
+                                handleSubmit={this.handleSubmit} 
+                                handleInputChange={this.handleInputChange}
+                                handleShowAll={this.handleShowAll}
+                                exitAdminMenu={this.props.exitAdminMenu} 
+                                handleAssociateSubmit={this.handleAssociateSubmit}
+                                allDates={this.state.allDates}/> 
+                            <AssociateForm />
+                        </div>
+                        <div className='card'>
+                            <button onClick={this.props.exitAdminMenu}>Exit Admin Menu</button>
+                        </div>
+                            
                     </div>               
                 )
             } else {
